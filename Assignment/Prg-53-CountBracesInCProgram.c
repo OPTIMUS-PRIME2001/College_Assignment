5001bbf1ff2c4e4c90a973e224815cc5e96d2cc3
PRIME_SAPTARSHI <chatterjeeu7@gmail.com>
2021-07-19 21:12:39 +0530

    C file

--- Assignment/Prg-53-CountBracesInCProgram.c
+++ Assignment/Prg-53-CountBracesInCProgram.c
@@ -3,27 +3,71 @@
      appear within comments, string literals, or character constants.
 */
 #include<stdio.h>
-int , count_clo;
+#include<stdlib.h>
+int o_parentheses = 0,o_sqbrackets = 0, o_curlybrackets = 0;
 void BracesCount(char ch)
 {
-    
+    if(ch=='(') o_parentheses++;
+    else if(ch==')')  
+    {  
+        if(o_parentheses!=0) o_parentheses--;
+        else return;
+    } 
+    else if(ch=='[') o_sqbrackets++;
+    else if(ch==']')  
+    {  
+        if(o_sqbrackets!=0) o_sqbrackets--;
+        else return;
+    } 
+    else if(ch=='{') o_curlybrackets++;
+    else if(ch=='}')  
+    {  
+        if(o_curlybrackets!=0) o_curlybrackets--;
+        else return;
+    } 
+    else 
+       return;
 }
 void main()
 {   
-    char code[1000][1000];
-    int i, j,n,len;
-    printf("\nEnter number of String : ");
-    scanf("%d",&n);
-    printf("\n"); 
-    for(i=0; i<n; i++)
+    int i, j,line,len;
+    printf("\nEnter number of lines : ");
+    scanf("%d",&line);
+    char **code = (char **)malloc((line) * sizeof(char *));
+    for(i=0; i<line; i++)    
+        code[i]=(char *)malloc(1000*sizeof(char));
+    printf("\033[0;32m");printf("Code Here > Terminal.c > main()\n");printf("\033[0m"); 
+    printf("%d |  #include <stdio.h>\n%d |  #include <stdlib.h>\n",1,2);
+    for(i=0; i<line; i++)
     {
-        printf("%d :  ",i+1);
-        code[i]=(char *)malloc(20*sizeof(char));
-        scanf("%s",code[i]);
-        len = sizeof(code[i])/sizeof(char);
-        for(j=0; j<len; j++)
+        fflush(stdin);
+        printf("%d |  ",i+3);
+        gets(code[i]);
+    }
+    for(i=0; i<line; i++)
+    {
+        for(j=0; code[i][j]!='\0'; j++)
         {
-            BracesCount(code[i][j]);
+            if(code[i][j]=='/' && code[i][j+1]=='/')
+                break;
+            else if(code[i][j]=='\"')
+            {
+                j++;
+                while(code[i][j]!='\"') j++;
+            }
+            else if(code[i][j]=='\'')
+                j=j+3;      
+            else 
+                BracesCount(code[i][j]);    
         }
     }
+    if(o_parentheses==0 && o_sqbrackets==0 && o_curlybrackets==0)
+        printf("\n Your Code is Perfect");
+    if(o_parentheses!=0)
+        printf("\nUnbalanced use of '(' (parentheses) in your code");
+    if(o_sqbrackets!=0)
+        printf("\nUnbalanced use of '[' (Square Brackets) in your code");
+    if(o_curlybrackets!=0)
+        printf("\nUnbalanced use of '{' (Curly Brackets) in your code");           
+    free(code);
 }
